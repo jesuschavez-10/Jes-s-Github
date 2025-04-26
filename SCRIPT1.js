caa17f9acb520bd9b7a006df98a0287dfef02232
jesuschavez-10 <71444267@certus.edu.pe>
2025-04-25 22:10:01 -0500

    vercion actual 25/04/25

---
+++ SCRIPT1.js
@@ -0,0 +1,13 @@
+function FCambioColor(){
+
+let vTexto1 = document.getElementById("ejemDOM1")
+vTexto1.textContent= "texto cambio por Funcion FCambioColor"; 
+vTexto1.style.color="blue";
+vTexto1.insertAdjacentHTML("betoreEnd")
+                       "<br> este texto es adicional <br>"
+    vTexto1.style.color="red" ;
+
+
+}
+
+

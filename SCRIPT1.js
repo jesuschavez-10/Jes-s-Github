14fcd8045e888ee3101aaeff2bff1d857c5b7d74
jesuschavez-10 <71444267@certus.edu.pe>
2025-05-02 22:10:04 -0500

    nueva clase

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -28,9 +28,51 @@ console.log("tipo de variable vTexto2",typeof vTexto2[1]);
 
 function FCambioTag1() {
     let vTexto3=document.getElementsByTagName('h2');
-    vTexto3[0],innerHTML="Elemento 1er cambiado por TagDom1";
-    vTexto3[6].innerHTML="Elemento ultimo h2 cambiado por TagDOM1";
+    vTexto3[0].innerHTML="Elemento 1er cambiado por TagDom1";
+    vTexto3[13].innerHTML="Elemento ultimo h2 cambiado por TagDOM1";
     vTexto3[13].style.color="magenta";
-    // SE COLOCA DE INDICE 6 POR SER EL 7MO ELEMENTO H2 DE LA PAG WED INDEX.HTML 
+    // indice 10 es el 11vo elemento h2
+    console.log("tamaño del array=",vTexto3.length);
+    vTexto3[13].insertAdjacentHTML("beforeEnd", "<br>tamaño de h2 =" + vTexto3.length) ;
+    console.log("Valor de totalh2=",totalh2);
+    console.log("tipo de variable totalh2=", typeof totalh2);
+}
+
+window.vTexto1="";
+
+function FAgregarTextoPermanente(){
+    let textoAdicional ="texto adicional para concatenar";
+    vTexto1 = vTexto1 + textoAdicional;
+    console.log("tipo de variable de vTexto1 =",typeof vTexto1);
+    console.log(vTexto1); 
+}
+
+function FcrearH1(){
+    var elemento =document.getElementById("idCrearH1");
+    var nuevoH1 =document.createElement("H1");
+    var texto = document.createTextNode("Texto colocado en linea");
+    nuevoH1.appendChild(texto);
+    elemento.appendChild(nuevoH1);
+}
+
+function FCapturarNombre(){
+
+    const vTexto1 = document.getElementById("nombre");
+    const vTexto2 = document.querySelectorAll("input.datosPersonales");
+    const vTexto3 = document.getElementById("clave") ;
+   // usar input para clases relacioandas a cajas de texto, etc
+    vTexto1.style.color="blue";
+    
+    var valor1 =vTexto1.value; // value obtiene el contenido de la caja de texto nombre
+    var valor2 ="";
+
+    document.getElementById("observacion").value = valor1;//textarea ya captura el nombre
+   //foreach recorre todo el contenido del array de la clase datosPersonales
+    vTexto2.forEach(input => {
+        valor2 =valor2 + `${input.name}: ${input.value}\n` ;
+      });
+
+    document.getElementById('observacion').value = valor2;
+    console.log('El valor del nombre es:', valor1);
 }
 
